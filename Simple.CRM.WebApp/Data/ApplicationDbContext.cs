1489c42266d2b84c5a82fcc609ce2725d63f0bcc
Shayne Meyer <shayne.meyer@gmail.com>
2025-10-14 16:24:53 -0700

    adding models

--- Simple.CRM.WebApp/Data/ApplicationDbContext.cs
+++ Simple.CRM.WebApp/Data/ApplicationDbContext.cs
@@ -15,4 +15,18 @@ public class ApplicationDbContext : IdentityDbContext<Agent>
     public DbSet<Customer> Customers { get; set; } = null!;
     public DbSet<VacationPackage> VacationPackages { get; set; } = null!;
     public DbSet<Booking> Bookings { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<VacationPackage>().HasData(
+            new VacationPackage
+            {
+                Id = 1,
+                Title = "Tropical Paradise",
+
+            }
+        );
+    }
 }
